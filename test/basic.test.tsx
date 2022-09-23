6b97bf3bf1ab2ecd686f5eae98a144ee8861d016
Amir.H Ebrahimi <amirhosseinebrahimi77@gmail.com>
2022-09-23 06:11:33 +0000

    feat: Replace spyConsole with vitest spyOn method
    - Remove the unused spyConsole utils file

--- test/basic.test.tsx
+++ test/basic.test.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { act, create } from 'react-test-renderer'
-import { describe, expect, it } from 'vitest'
-
-import { spyConsole } from './utils'
+import { afterEach, describe, expect, it, vi } from 'vitest'
 
 import useTilg from '../src/development'
 
 describe('useTilg', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
   it('should log the render, mount and unmount events', async () => {
-    const [logs, reset] = spyConsole()
+    const logSpy = vi.spyOn(console, 'log')
 
     function App() {
       useTilg()
@@ -17,20 +19,21 @@ describe('useTilg', () => {
     const app = create(<App />)
     app.toJSON()
     act(() => app.unmount())
-    reset()
 
-    expect(logs[0][0]).toEqual(
-      '\x1B[96m\x1B[1m<App/>\x1B[39m\x1B[22m rendered with props: \x1B[96m\x1B[1m%o\x1B[39m\x1B[22m.'
+    expect(logSpy.mock.calls[0][0]).toEqual(
+      '\x1B[96m\x1B[1m<App/>\x1B[39m\x1B[22m rendered with props: \x1B[96m\x1B[1m%o\x1B[39m\x1B[22m.',
     )
-    expect(logs[0][1]).toEqual({})
-    expect(logs[1][0]).toEqual('\x1B[96m\x1B[1m<App/>\x1B[39m\x1B[22m mounted.')
-    expect(logs[2][0]).toEqual(
-      '\x1B[96m\x1B[1m<App/>\x1B[39m\x1B[22m unmounted.'
+    expect(logSpy.mock.calls[0][1]).toEqual({})
+    expect(logSpy.mock.calls[1][0]).toEqual(
+      '\x1B[96m\x1B[1m<App/>\x1B[39m\x1B[22m mounted.',
+    )
+    expect(logSpy.mock.calls[2][0]).toEqual(
+      '\x1B[96m\x1B[1m<App/>\x1B[39m\x1B[22m unmounted.',
     )
   })
 
   it('should log the message', async () => {
-    const [logs, reset] = spyConsole()
+    const logSpy = vi.spyOn(console, 'log')
 
     function App() {
       useTilg()`The we we are`
@@ -39,13 +42,12 @@ describe('useTilg', () => {
     const app = create(<App />)
     app.toJSON()
     act(() => app.unmount())
-    reset()
 
-    expect(logs[1][0]).toContain('The we we are')
+    expect(logSpy.mock.calls[1][0]).toContain('The we we are')
   })
 
   it('should log number arguments', async () => {
-    const [logs, reset] = spyConsole()
+    const logSpy = vi.spyOn(console, 'log')
 
     function App() {
       useTilg()`The answer is ${42}`
@@ -54,13 +56,12 @@ describe('useTilg', () => {
     const app = create(<App />)
     app.toJSON()
     act(() => app.unmount())
-    reset()
 
-    expect(logs[1][0]).toContain('The answer is 42')
+    expect(logSpy.mock.calls[1][0]).toContain('The answer is 42')
   })
 
   it('should log string arguments', async () => {
-    const [logs, reset] = spyConsole()
+    const logSpy = vi.spyOn(console, 'log')
 
     function App() {
       useTilg()`The answer is ${'42'}`
@@ -69,13 +70,12 @@ describe('useTilg', () => {
     const app = create(<App />)
     app.toJSON()
     act(() => app.unmount())
-    reset()
 
-    expect(logs[1][0]).toContain('The answer is "42"')
+    expect(logSpy.mock.calls[1][0]).toContain('The answer is "42"')
   })
 
   it('should log object arguments', async () => {
-    const [logs, reset] = spyConsole()
+    const logSpy = vi.spyOn(console, 'log')
 
     function App() {
       useTilg()`The answer is ${{ answer: 42 }}`
@@ -84,15 +84,14 @@ describe('useTilg', () => {
     const app = create(<App />)
     app.toJSON()
     act(() => app.unmount())
-    reset()
 
-    expect(logs[1][0]).toContain('The answer is %o')
-    expect(logs[1][1]).toEqual({ answer: 42 })
+    expect(logSpy.mock.calls[1][0]).toContain('The answer is %o')
+    expect(logSpy.mock.calls[1][1]).toEqual({ answer: 42 })
   })
 
   describe('props', () => {
     it('should log props', async () => {
-      const [logs, reset] = spyConsole()
+      const logSpy = vi.spyOn(console, 'log')
 
       function App({ answer }) {
         useTilg()
@@ -101,13 +100,12 @@ describe('useTilg', () => {
       const app = create(<App answer={42} />)
       app.toJSON()
       act(() => app.unmount())
-      reset()
 
-      expect(logs[0][1]).toEqual({ answer: 42 })
+      expect(logSpy.mock.calls[0][1]).toEqual({ answer: 42 })
     })
 
     it('should log props updates', async () => {
-      const [logs, reset] = spyConsole()
+      const logSpy = vi.spyOn(console, 'log')
 
       function App({ answer }) {
         useTilg()
@@ -117,10 +115,9 @@ describe('useTilg', () => {
       app.toJSON()
       act(() => app.update(<App answer={43} />))
       act(() => app.unmount())
-      reset()
 
-      expect(logs[0][1]).toEqual({ answer: 42 })
-      expect(logs[2][1]).toEqual({ answer: 43 })
+      expect(logSpy.mock.calls[0][1]).toEqual({ answer: 42 })
+      expect(logSpy.mock.calls[2][1]).toEqual({ answer: 43 })
     })
   })
 })
